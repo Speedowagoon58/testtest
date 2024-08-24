92ee1bb155527ed80e1698d31de965b6d0b84b1c
Speedowagoon58 <100315622+Speedowagoon58@users.noreply.github.com>
2024-08-24 19:23:03 +0100

    testing

--- main.go
+++ main.go
@@ -3,17 +3,21 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	// Serve static files from the "static" directory
-	fs := http.FileServer(http.Dir("static"))
-	http.Handle("/", fs)
+    fs := http.FileServer(http.Dir("static"))
+    http.Handle("/", fs)
 
-	// Start the server on port 8080
-	log.Println("Server is running on http://localhost:8080")
-	err := http.ListenAndServe("0.0.0.0:8080", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+    port := os.Getenv("PORT")
+    if port == "" {
+        port = "8080"
+    }
+
+    log.Printf("Server is running on port %s", port)
+    err := http.ListenAndServe(":"+port, nil)
+    if err != nil {
+        log.Fatal(err)
+    }
 }
